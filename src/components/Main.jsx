64128fbcada952af24d2d1c6369cf8a6aa0585cf
Ayoub ELGOUNTARI <elgountariayoub21@gmail.com>
2025-04-10 18:52:32 +0100

    adding Mistral AI integration to generate the recipre based on the giving ingredients

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import IngredientsList from "./IngredientsList";
+import { getRecipeFromMistral } from "../ai";
 
 export default function Main() {
   const [ingredients, setIngredients] = React.useState([]);
+  const [recipe, setRecipe] = React.useState("");
 
   function addIngredient(formData) {
     const newIngredient = formData.get("ingredient");
     setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
+
+  async function getRecipe() {
+    const result = await getRecipeFromMistral(ingredients);
+    setRecipe(result);
+    console.log(result);
   }
 
   return (
@@ -21,7 +29,7 @@ export default function Main() {
         <button>Add ingredient</button>
       </form>
       {ingredients.length > 0 && (
-        <IngredientsList ingredients={ingredients} />
+        <IngredientsList ingredients={ingredients} handleClick={getRecipe} />
       )}
     </main>
   );
